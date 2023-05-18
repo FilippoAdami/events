4265fcbe060bffe095982c7777163d4334d79ab8
matteogrisenti <matteo.grisenti@studenti.unitn.it>
2023-05-18 14:11:44 +0200

    Piccole Modifiche
    sui Modelli e Swagger UI

--- server/models/bachecaM.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const bachecaSchema = new mongoose.Schema({
-    numeroInserzioni : {type : Number, required : true},
-    tipoInserzioni : {},
-    frequPubblicita : {type : Number, required : true}
-}, {collection : 'bacheca'});
-
-module.exports = mongoose.model('Bacheca',bachecaSchema);
