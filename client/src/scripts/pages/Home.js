04c3c083c04bff99071a474b5a3de2baef11b37d
sabe <matteo.sabella@live.it>
2023-06-01 12:01:45 +0200

    fixed elements

--- client/src/scripts/pages/Home.js
+++ client/src/scripts/pages/Home.js
@@ -3,7 +3,6 @@ import Header from '../components/header';
 import Footer from '../components/footer';
 import Grid from '../components/grid';
 import Toggle from '../components/toggle.js';
-import MapC from "../subcomponents/map.js"
 
 
 import Cookies from 'js-cookie'; 
@@ -25,8 +24,6 @@ function Home() {
 
   return (
     <>
-      <h1>TEST HEADER</h1>
-      <MapC />
       <Header />
       <Toggle onToggle={handleToggle}/>
       <Grid selectedOption={selectedOption}/>
